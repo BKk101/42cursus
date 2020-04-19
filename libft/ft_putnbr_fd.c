34bc5b7730c65785374a7ec68bb35d60a5bc2667
bykim <bykim@student.42seoul.kr>
2020-04-19 22:06:05 +0900

    2nd edit

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -6,17 +6,37 @@
 /*   By: bykim <bykim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/01 01:12:56 by bykim             #+#    #+#             */
-/*   Updated: 2020/04/19 21:44:24 by bykim            ###   ########.fr       */
+/*   Updated: 2020/04/19 22:05:17 by bykim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+void	putnbr(long long n, int fd)
+{
+	int remainder;
+
+	if (n == 0)
+		return ;
+	remainder = n % 10;
+	putnbr(n / 10, fd);
+	write(fd, remainder, 1);
+}
+
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*str;
+	long long	temp;
 
-	str = ft_itoa(n);
-	write(fd, str, ft_strlen(str));
-	free(str);
+	if (n == 0)
+	{
+		write(fd, n, 1);
+		return ;
+	}
+	temp = n;
+	if (temp < 0)
+	{
+		temp *= -1;
+		write(fd, '-', 1);
+	}
+	putnbr(temp, fd);
 }
